4d66b07586b2a9c09951253721c276608f6c6f50
lihuizhen <huizhen.li@charleskeith.com>
2020-09-09 15:26:33 +0800

    feat: 安装componsiton-api 体验vue3.0新特性

---
+++ src/components/1.setup.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>测试setup</div>
+</template>
+<script>
+export default {
+  name: "",
+  data() {
+    return {};
+  },
+  components: {},
+  props: {},
+  computed: {},
+  methods: {},
+  beforeCreate() {
+    console.log("this is beforeCreate");
+  },
+  setup() {
+    // setup函数在生命周期函数beforeCreate和created之间执行
+    console.log("this is setup");
+  },
+  created() {
+    console.log("this is created");
+  },
+  mounted() {},
+  watch: {}
+};
+</script>
+
+<style lang="less" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ComSetup />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import ComSetup from "@/components/1.setup.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    ComSetup
   }
 };
 </script>
