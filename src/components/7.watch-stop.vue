32d9668b18868c0fcdfad312e6b899f9aa6456af
Jenny <18839970925@163.com>
2020-09-13 17:44:33 +0800

    feat: 停止监听

---
+++ src/components/7.watch-stop.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>------watch stop------</h2>
+    <p>count的值：{{ count }}</p>
+    <button @click="stop">停止监听</button>
+  </div>
+</template>
+
+<script>
+import { ref, watch } from "@vue/composition-api";
+export default {
+  setup() {
+    console.log("--------------watch stop--------------");
+    const count = ref(0);
+
+    // 直接调用stop()便可停止监听
+    const stop = watch(count, () => console.log(count.value));
+
+    setInterval(() => {
+      count.value += 1;
+    }, 2000);
+
+    return {
+      count,
+      stop
+    };
+  }
+};
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="about">
     <ComWatch />
+    <ComWatchStop />
   </div>
 </template>
 
 <script>
 import ComWatch from "@/components/6.watch.vue";
+import ComWatchStop from "@/components/7.watch-stop.vue";
 export default {
-  components: { ComWatch }
+  components: { ComWatch, ComWatchStop }
 };
 </script>
