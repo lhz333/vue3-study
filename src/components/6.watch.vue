a48ca7f1ccc7ad595f642bb04fd58724e308f2bb
lhz333 <18839970925@163.com>
2020-09-15 11:06:38 +0800

    feat: 修改watch文件

--- src/components/6.watch.vue
+++ src/components/6.watch.vue
@@ -4,7 +4,7 @@
     <p>count的值：{{ count }}</p>
     <button @click="add">add</button>
     <p>name：{{ name }}</p>
-    <input type="text" @blur="changeInput" />
+    <input type="text" v-model="name" />
   </div>
 </template>
 
@@ -17,24 +17,22 @@ import {
   toRefs
 } from "@vue/composition-api";
 export default {
-  setup() {
+  setup () {
     console.log("--------------watch--------------");
     const count = ref(0);
 
     const add = () => (count.value += 1);
 
     const state = reactive({
-      name: "reactive"
+      name: "reactive",
+      sex: "女"
     });
-    const changeInput = e => {
-      state.name = e.target.value;
-    };
-    // watchEffect() 监听props
+    // watchEffect的用法与watch有所不同，watchEffect会传入一个函数，然后立即执行这个函数，对于函数里面的响应式依赖会进行监听，然后当依赖发生变化时，会重新调用传入的函数
     watchEffect(() => {
       console.log(count.value);
       console.log(state.name);
     });
-    // watch监听器
+    // watch除了可以监听单个值或者函数返回值之外，也可以同时监听多个数据源
     watch(
       // 这里是要监听的数据 count是ref创建的数据，() => state.name是reactive创建的数据
       [count, () => state.name],
@@ -43,11 +41,21 @@ export default {
         console.log(newValue, oldValue);
       }
     );
+
+    // watch 可以监听一个函数的返回值
+    watch(
+      () => {
+        return state.name + state.sex;
+      },
+      value => {
+        // state中name或者sex发生变化时，都会进入这个函数
+        console.log(`名字加姓别是：${value}`);
+      }
+    );
     return {
       ...toRefs(state),
       count,
-      add,
-      changeInput
+      add
     };
   }
 };
