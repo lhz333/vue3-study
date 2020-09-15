4c33c787635976a7c9c4bfb3147b9265b79c6bc2
lhz333 <18839970925@163.com>
2020-09-15 18:01:11 +0800

    feat: template refs 组件的引用

---
+++ src/components/refs/son.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>子组件</h3>
+
+    <p>count的值是：{{ count }}</p>
+    <button @click="count += 1">+1</button>
+  </div>
+</template>
+<script>
+import { ref } from "@vue/composition-api";
+export default {
+  setup() {
+    const count = ref(0);
+
+    return {
+      count
+    };
+  }
+};
+</script>
